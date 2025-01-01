8781957b4b168f74fc2d97c7b3b8b4d0db11d8d8
shaiksha176 <iamshaiksha17@gmail.com>
2025-01-01 22:51:49 +0530

    first commit

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(morgan("dev"));
 
 // connect to MongoDB database
